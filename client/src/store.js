8f07c960d86c6bc4bf4128f2bf0983d82490bc57
hyewon do <tsc03018@gmail.com>
2020-11-28 23:12:07 +0900

    client settings -redux

---
+++ client/src/store.js
@@ -0,0 +1,15 @@
+import rootSaga from './saga/index';
+import rootReducer from './reducers/index';
+import {applyMiddleware, createStore} from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import {composeWithDevTools} from 'redux-devtools-extension';
+
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(
+    rootReducer,composeWithDevTools(applyMiddleware(sagaMiddleware))
+)
+
+sagaMiddleware.run(rootSaga);
+
+export default store;
