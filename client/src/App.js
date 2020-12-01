34c312e3cd13027b1d10623f055be620add9e61e
hyewon do <tsc03018@gmail.com>
2020-12-01 18:38:37 +0900

    client settings

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,17 @@
 import './App.scss';
+import Nav from './components/Nav';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import CalendarPage from './components/CalendarPage';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App"> 
+      <Router>
+        <Nav />
+        <Switch>
+          <Route path="/" component={CalendarPage} />
+        </Switch>
+      </Router>
     </div>
   );
 }
