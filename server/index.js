46517413a2ca434ceb3b21f352352243cb97b024
hyewon do <tsc03018@gmail.com>
2020-12-11 18:28:01 +0900

    heroku 연습...

--- server/index.js
+++ server/index.js
@@ -23,11 +23,11 @@ app.use(cookieParser());
 
 if (process.env.NODE_ENV === "production") {
 //"client/build"는 react의 build파일 경로이다
-  app.use(express.static("../client/build"));
+  app.use(express.static("./client/build"));
 
 //"..client"는 react 프로젝트의 파일 경로, "build"는 react프로젝트 내의 build폴더이다
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "./client", "build", "index.html"));
   });
 }
 
